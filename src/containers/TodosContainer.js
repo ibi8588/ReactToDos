2e417eeb05712b68293f8ae739a3ec79143c703a
ibi8588 <ed8588@gmail.com>
2017-06-12 11:17:07 -0700

    delete complete, but bugged

--- src/containers/TodosContainer.js
+++ src/containers/TodosContainer.js
@@ -21,6 +21,15 @@ class TodosContainer extends Component {
       })
     })
   }
+  deleteTodo(todo) {
+  console.log('deleting todo', todo)
+  TodoModel.delete(todo).then((res) => {
+      let todos = this.state.todos.filter(function(todo) {
+        return todo._id !== res._id
+      });
+      this.setState({todos})
+  })
+}
 
   createTodo(newBody) {
   let newTodo = {
@@ -40,7 +49,8 @@ class TodosContainer extends Component {
         <CreateTodoForm
           createTodo={this.createTodo.bind(this)}/>
         <TodoList
-          todos={this.state.todos} />
+          todos={this.state.todos}
+          onDeleteTodo={this.deleteTodo.bind(this)}/>
       </div>
     )
   }
